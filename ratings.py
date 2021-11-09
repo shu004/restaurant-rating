60e6f90b0bdd639ce9afb0d275f67db976dc95f8
Sisi Hu <shu004@gmail.com>
2021-11-08 18:41:03 -0700

    reorganized code

--- ratings.py
+++ ratings.py
@@ -1,24 +1,43 @@
 """Restaurant rating lister."""
+FILE_NAME = "scores.txt"
 
+def restaurant_rating(FILE_NAME):
+    #this returns a list from user input
+    user_rating = user_input()
 
-# put your code here
-def restaurant_rating(filename):
-    user_name = input("Enter restaurant name:")
-    user_score = input("Enter score:")
+    #this combs through the FILE_NAME and add items to a dictionary
+    rest_data = file_to_dict(FILE_NAME)
 
-#empty dictionary, open file name, unpack list
-    rest_data = {}
+    #adding user input to the restaurant data dictionary
+    user_input_name, user_input_score = user_rating
+    rest_data[user_input_name] = user_input_score
+
+    sorted_rest_data = sorted((rest_data.items()))
+    for restaurant in sorted_rest_data:
+        restaurant_name = restaurant[0]
+        restaurant_rating = restaurant[1]
+
+        print(f"{restaurant_name} is rated at {restaurant_rating}")
+
+def file_to_dict(filename):
+    new_dict = {}
     with open(filename) as file:
         for line in file:
-            name, rating = line.rstrip().split(":")
-            rest_data[name] = rating
+            key, value = line.rstrip().split(":")
+            new_dict[key] = value
 
-        rest_data[user_name] = user_score
+    return new_dict
 
-        sorted_rest_data = sorted((rest_data.items()))
-        for restaurant in sorted_rest_data:
-            restaurant_name = restaurant[0]
-            restaurant_rating = restaurant[1]
 
-            print(f"{restaurant_name} is rated at {restaurant_rating}")
+def user_input():
+    ask_for_input = False
+    while not ask_for_input:
+        user_name = input("Enter restaurant name:").title()
+        user_score = int(input("Enter score:"))
+        if user_score < 1 or user_score > 5:
+            print("Not a valid input, please enter again")
+        else:
+            ask_for_input = True
+    return [user_name,user_score]
 
+restaurant_rating(FILE_NAME)
